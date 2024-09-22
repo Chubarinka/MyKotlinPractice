913773dda2256773afb161d318d206be6c00baae
alina <alina.chubar@mail.ru>
2024-09-22 22:28:41 +0400

    MKP-1-3 create practice1 task3

---
+++ src/main/kotlin/practice1_variables/practice1_task3.kt
@@ -0,0 +1,21 @@
+package org.example.practice1_variables
+
+fun main() {
+
+    val year = 1961
+    var hour=9
+    var minute=7
+
+    println("Год полета:")
+    println(year)
+    println("Взлет:")
+    println("Час:")
+    println(hour)
+    println("Минута:")
+    println(minute)
+
+    hour=10
+    minute=55
+    println("Посадка:")
+    println("$hour:$minute")
+}
