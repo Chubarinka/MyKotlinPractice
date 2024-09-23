69f6963e33995e0e25344619fbdf3dc60a76bf22
alina <alina.chubar@mail.ru>
2024-09-23 16:23:01 +0400

    MKP-1-5 changed practice1 task5

--- src/main/kotlin/practice1_variables/practice1_task5.kt
+++ src/main/kotlin/practice1_variables/practice1_task5.kt
@@ -1,11 +1,15 @@
 package org.example.practice1_variables
 
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_HOUR = 3600
 fun main() {
-    val totalSecond=5409
 
-    val hours = totalSecond/3600
-    val minutes = totalSecond%3600/60
-    val seconds = totalSecond%60
+    val totalSecondGagarinInSpace = 6480
+
+    val hours = totalSecondGagarinInSpace / SECONDS_IN_HOUR
+    val minutes = totalSecondGagarinInSpace % SECONDS_IN_HOUR / MINUTES_IN_HOUR
+    val seconds = totalSecondGagarinInSpace % MINUTES_IN_HOUR
+
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
