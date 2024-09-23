21ff245ce95170cf3bf8dea904f334294eca5d9b
alina <alina.chubar@mail.ru>
2024-09-23 15:39:01 +0400

    MKP-1-1 changed practice1 task1

--- src/main/kotlin/practice1_variables/practice1_task1.kt
+++ src/main/kotlin/practice1_variables/practice1_task1.kt
@@ -2,7 +2,7 @@ package org.example.practice1_variables
 
 fun main() {
 
-    var ordersCount: Int =75
-    val thankMessage : String ="Спасибо, что выбираете нас!"
+    var ordersCount: Int = 75
+    val thankMessage: String = "Спасибо, что выбираете нас!"
 
 }
